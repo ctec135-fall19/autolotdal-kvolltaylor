4cfe9ba8f4a577814d6ee53383a9a6af2e402c2b
Katrina <k.voll-taylor@students.clark.edu>
2019-11-11 06:04:24 -0800

    DAL Example submission

--- Module7-BonusDALExample/AutoLotClientApp/Program.cs
+++ Module7-BonusDALExample/AutoLotClientApp/Program.cs
@@ -25,6 +25,8 @@ namespace AutoLotClientApp
 
             InventoryDAL dal = new InventoryDAL();
             var list = dal.GetAllInventory();
+
+            // list all cars in the inventory
             Console.WriteLine((new string('*', 15)) + " All Cars " + (new string('*', 15)));
             Console.WriteLine("CarId\tMake\tColor\tPet Name");
             foreach (var itm in list)
@@ -32,11 +34,15 @@ namespace AutoLotClientApp
                 Console.WriteLine($"{itm.CarId}\t{itm.Make}\t{itm.Color}\t{itm.PetName}");
             }
             Console.WriteLine();
+
+            // list the first car in order of color
             var car = dal.GetCar(list.OrderBy(x => x.Color).Select(x => x.CarId).First());
             Console.WriteLine((new string('*', 15)) + " First Car By Color " + (new string('*', 15)));
             Console.WriteLine("CarId\tMake\tColor\tPet Name");
             Console.WriteLine($"{car.CarId}\t{car.Make}\t{car.Color}\t{car.PetName}");
+            Console.WriteLine();
 
+            // try/catch logic to delete a car in inventory
             try
             {
                 dal.DeleteCar(5);
@@ -46,6 +52,9 @@ namespace AutoLotClientApp
             {
                 Console.WriteLine($"An exception occurred: {ex.Message}");
             }
+            Console.WriteLine();
+
+            // insert a new car object into the inventory
             dal.InsertAuto(new Car { Color = "Blue", Make = "Pilot", PetName = "TowMonster" });
             list = dal.GetAllInventory();
             var newCar = list.First(x => x.PetName == "TowMonster");
@@ -53,6 +62,16 @@ namespace AutoLotClientApp
             Console.WriteLine("CarId\tMake\tColor\tPet Name");
             Console.WriteLine($"{newCar.CarId}\t{newCar.Make}\t{newCar.Color}\t{newCar.PetName}");
             dal.DeleteCar(newCar.CarId);
+            Console.WriteLine();
+
+            // list the inventory again
+            Console.WriteLine((new string('*', 15)) + " All Cars After an Insertion & a Deletion " + (new string('*', 15)));
+            Console.WriteLine("CarId\tMake\tColor\tPet Name");
+            foreach (var itm in list)
+            {
+                Console.WriteLine($"{itm.CarId}\t{itm.Make}\t{itm.Color}\t{itm.PetName}");
+            }
+            Console.WriteLine();
 
             Console.WriteLine("Press enter to continue...");
             Console.ReadLine();
